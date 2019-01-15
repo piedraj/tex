a8a5238b8179d421b6a26dacaab893efdc2488b4
Jonatan Piedra <jonatan.piedra@gmail.com>
2019-01-15 16:12:24 +0100

    Pregunta de examen 2019 lista

--- LFI/compile.sh
+++ LFI/compile.sh
@@ -4,6 +4,7 @@ if [ $# -lt 1 ]; then
     echo "  "
     echo " ./compile.sh LFI-Resonancia.tex"
     echo " ./compile.sh LFI-Resonancia-Cuestionario.tex"
+    echo " ./compile.sh LFI-Examen-2019.tex"
     echo "  "
     exit -1
 fi
